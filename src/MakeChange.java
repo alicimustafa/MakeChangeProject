946ffd1efa818b7d1dd719bcb6264e9af4d43e82
mustafa alici <mustafaalici@mustafas-MacBook-Pro.local>
2017-08-04 15:40:23 -0600

    mehtod stubs

--- src/MakeChange.java
+++ src/MakeChange.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
 
 public class MakeChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner keyboard = new Scanner(System.in);
+		runRegister(keyboard);
 	}
+	
+	public static void runRegister(Scanner keyboard) {
+		int priceInpennies = getInputFromUser(keyboard);
+		
+	}
+	
+	public static int getInputFromUser(Scanner keyboard) {
+		
+		return 0;
+	}
+	
+	public static int figureHowMuchOfEach(int total, int dem) {
+		
+		return 0;
+	}
+	
+	
 
 }
