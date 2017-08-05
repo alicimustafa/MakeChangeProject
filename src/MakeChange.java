cf38a22cb1e47d1ecab0fc5d552ff94545b0bfd8
mustafa alici <mustafaalici@mustafas-MacBook-Pro.local>
2017-08-04 19:41:35 -0600

    added another method stub and added more stuff to runRegister

--- src/MakeChange.java
+++ src/MakeChange.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 
+/**
+ * @author mustafaalici
+ *
+ */
 public class MakeChange {
 
 	public static void main(String[] args) {
@@ -7,15 +11,36 @@ public class MakeChange {
 		runRegister(keyboard);
 	}
 	
+	/**
+	 * @param keyboard
+	 */
 	public static void runRegister(Scanner keyboard) {
-		System.out.println("Please enter the price of item: ");
-		int priceInpennies = getInputFromUser(keyboard);
-		System.out.println((priceInpennies / 100.0));
+
+		System.out.print("Please enter the price of item: ");
+		int priceInPennies = getInputFromUser(keyboard);
+		System.out.println((priceInPennies / 100.0));
+		System.out.print("How much money do want to give me");
+		int moneyInPennies = getInputFromUser(keyboard);
+		System.out.println(moneyInPennies / 100.0);
+		while(priceInPennies > moneyInPennies) {
+			System.out.println("Hey budy this aint a cherity you need enuf money to pay for it");
+			System.out.print("Give me the correct amount ");
+			moneyInPennies = getInputFromUser(keyboard);
+		}
+		
 	}
 	
+	/**
+	 * @param keyboard
+	 * @return
+	 */
 	public static int getInputFromUser(Scanner keyboard) {
 		double input = keyboard.nextDouble();
 		return (int)(input * 100);
+	}
+	
+	public static void displayChange(int change) {
+		
 	}
 	
 	public static int figureHowMuchOfEach(int total, int dem) {
